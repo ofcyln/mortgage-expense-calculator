2127437ec7c741a009bfbb538f3ef3d452b28aa0
Osman Fikret Ceylan <ofcyln@gmail.com>
2020-05-10 21:30:21 +0200

    Share button component integration, Mixin SCSS file integration, COmponent module relation opmimisations

---
+++ src/app/core/header/share-buttons/share-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-share-buttons',
+  templateUrl: './share-buttons.component.html',
+  styleUrls: ['./share-buttons.component.scss']
+})
+export class ShareButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
