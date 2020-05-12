30590f21b18f00ea62bba53180e7f4af34cea582
ofcyln <ofcyln@gmail.com>
2020-05-12 15:39:15 +0200

    Input number and decimal keyboard support added for mobile devices

--- src/app/components/calculate/calculate.component.ts
+++ src/app/components/calculate/calculate.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './calculate.component.scss' ]
 })
 export class CalculateComponent implements OnInit {
-  value = '';
+  value = null;
 
   constructor() {
   }
